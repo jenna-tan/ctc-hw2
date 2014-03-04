d9b074302527a19f3ba3b61485116bfeef11e917
Jenna Tan <jenna.r.tan@gmail.com>
2014-03-04 14:26:54 -0800

    Add files for Homework 2

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 $(document).ready(function(){ 
  //INSERT CODE HERE
-
+ 	
  //QUESTION 1
+    function Dog () {
+        this.bark = function() {console.log("woof!");}
+    	}
+
+	function ShibaInu() {}
+	ShibaInu.prototype = new Dog();
+	ShibaInu.prototype.constructor = ShibaInu;
+	ShibaInu.prototype.bark = function () { console.log("much wow very javascript"); }
+
+
 
  //QUESTION 3
+	$('.secondbutton').click(function() {
+		$('.secondheading').html("Dogs are the best...");
+		$(this).css({'background': 'rgb(175, 238, 238)'});
+	});
+
+	
 
  //QUESTION 4
 
+	$("#task4").click(function() {
+		$("#hipcat").css({"-webkit-filter": "blur(2px) sepia(0.5)"});
+	});
+
  //QUESTION 5
 
+	$("#car").click(function(){
+		$( ".wheel" ).css({'-webkit-animation-name' : 'spin',
+			'-webkit-animation-play-state' : 'running'});
+		$( ".spincar" ).css({'-webkit-animation-play-state' : 'running'});
+	});
+
  //QUESTION 6
+ 	$(".nyancat").animate({
+ 		left: "900"
+ 	}, 5000, function(){
+ 	});
+ 	$(".rainbows").animate({
+ 		width: "+=800"
+ 	}, 5500, function(){
+ 	});
+
 });
